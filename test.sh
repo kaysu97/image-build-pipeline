046f85f017bbf2e5a91034adb7da37dac9ccf0ab
Kay Su <kay85esb@gmail.com>
2022-12-02 14:01:15 +0800

    add job for matrix vars

--- test.sh
+++ test.sh
@@ -1,14 +1,16 @@
 filename='base_image_list.txt'
-IFS='-'
+# IFS='-'
+image_list="["
 while read image_tag || [ -n "$image_tag" ]; do
-    read -a strarr <<< "$image_tag"
-    len=$((${#strarr[*]}-3))
-    sep_arr=${strarr[*]:2:len}
-    file_name=`echo "$sep_arr" | sed 's/ /-/g'`
-    dockerfile="Dockerfile.$file_name"
-    echo "$dockerfile"
-    airflow_tag=`echo ${strarr[*]::2} | sed 's/ /-/g'`
-    echo "$airflow_tag"
-    docker build . -f "Dockerfile.base" -t coolcol97/"$image_tag" --build-arg AIRFLOW_IMAGE_TAG="$airflow_tag"
-# docker push $IMAGE_REGISTRY:$image_tag
+    image_list="$image_list$image_tag, "
+    # len=$((${#strarr[*]}-3))
+    # sep_arr=${strarr[*]:2:len}
+    # file_name=`echo "$sep_arr" | sed 's/ /-/g'`
+    # dockerfile="Dockerfile.$file_name"
+    # echo "dockerfile name: $dockerfile"
+    # airflow_tag=`echo ${strarr[*]::2} | sed 's/ /-/g'`
+    # echo "airflow tag: $airflow_tag"
+    # echo "image tag: $image_tag"
+    # docker build . -f "$dockerfile" -t "coolcool97"/"$image_tag" --build-arg AIRFLOW_IMAGE_TAG="$airflow_tag"
+    # # docker push $IMAGE_REGISTRY/$image_tag
 done < $filename
